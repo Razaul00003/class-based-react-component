aacc8e8050977af8d0755a2ccbc6c4c1f65f6311
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-09-29 18:31:52 +0600

    implementing filtering

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,35 +1,33 @@
 import { Component } from "react";
-import User from "./User";
 
+import User from "./User";
 import classes from "./Users.module.css";
 
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
-
-///class based components
 class Users extends Component {
   constructor() {
     super();
     this.state = {
-      showUsers: false,
+      showUsers: true,
+      more: "Test",
     };
   }
+
   toggleUsersHandler() {
+    // this.state.showUsers = false; // NOT!
     this.setState((curState) => {
       return { showUsers: !curState.showUsers };
     });
   }
+
   render() {
     const usersList = (
       <ul>
-        {DUMMY_USERS.map((user) => (
+        {this.props.users.map((user) => (
           <User key={user.id} name={user.name} />
         ))}
       </ul>
     );
+
     return (
       <div className={classes.users}>
         <button onClick={this.toggleUsersHandler.bind(this)}>
@@ -40,5 +38,4 @@ class Users extends Component {
     );
   }
 }
-
 export default Users;
